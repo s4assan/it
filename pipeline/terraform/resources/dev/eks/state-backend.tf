412e4ae6b58d9ff4b9a07f55204fc850f9556e9d
Tia Leo <tia@bankofamerica.com>
2022-11-20 22:21:53 -0600

    terraform

--- pipeline/terraform/resources/dev/eks/state-backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "tia-terraform-state-backend"
-    key    = "terraform-backend-state/eks-control-plane"
-    region = "us-east-1"
-    # dynamodb_table = "terraform_state"
-  }
-}
--- pipeline/terragrunt/dev/vpc1/.terragrunt-cache/_HqRNFmS5DyMPV_onGYJiegQKXg/24u9AAb0SwJbnzm2dOD28I7pUUQ/example/state-backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "tia-terraform-state-backend"
-    key    = "terraform-backend-state/vpc/mam-dev"
-    region = "us-east-1"
-    # dynamodb_table = "terraform_state"
-  }
-}
